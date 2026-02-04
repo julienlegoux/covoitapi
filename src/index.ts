a3ed609f0ece1a2a8ff2e34516f88678d02e13f0
Julien Legoux <lgx.julien@gmail.com>
2026-02-04 10:10:24 +0100

    fix vercel

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import "reflect-metadata";
 import "dotenv/config";
+import { Hono } from "hono";
 import { app } from "./presentation/routes/index.js";
 import { logger } from "./lib/shared/utils/logger.util.js";
 import "./infrastructure/di/container.js";
+
+// Required for Vercel to detect this as a Hono app
+void Hono;
 
 logger.info("Server initialized", { environment: process.env.NODE_ENV });
 
